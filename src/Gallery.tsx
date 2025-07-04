2bbc35c68dd79ff07e38a082f26643c92842ea7f
tako <cmutnik@gmail.com>
2025-07-04 14:48:54 -0400

    static images

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './Gallery.css';
 
 const images = [
-  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/gh-pages/images/1_resized.webp',
-  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/gh-pages/images/2_resized.webp',
-  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/gh-pages/images/3.png',
-  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/gh-pages/images/4_resized.webp',
-  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/gh-pages/images/5_resized.webp',
-  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/gh-pages/images/6_resized.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/1.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/2.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/3.png',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/4.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/5.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/6.png',
 ];
 
 function Gallery() {
