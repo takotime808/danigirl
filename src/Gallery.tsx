e9cfbc3b53e099c0b84435eef7e43b2a985feef3
tako <cmutnik@gmail.com>
2025-07-04 17:00:38 -0400

    rotating logo

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -8,6 +8,18 @@ const images = [
   'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/4.webp',
   'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/5.webp',
   'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/6.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/7.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/8.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/9.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/10.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/11.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/12.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/13.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/14.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/15.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/16.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/17.webp',
+  'https://raw.githubusercontent.com/takotime808/danigirl/refs/heads/main/images/18.webp',
 ];
 
 function Gallery() {
